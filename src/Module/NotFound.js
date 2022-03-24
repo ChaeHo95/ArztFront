e546d6f104b9c8fd0bc5348f67eb21984d7dad83
Chan Hyeon Baek <92700067+C-H2Baek@users.noreply.github.com>
2022-03-24 14:02:13 +0900

    chan

--- src/Module/NotFound.js
+++ src/Module/NotFound.js
@@ -3,22 +3,22 @@ import { Container } from 'react-bootstrap';
 import Footer from '../Layout/Footer';
 
 const Global = () => {
-  return(
-    
-      <Container>
-        <section class="vh-100 bg-washed-blue baskerville">
-          <header class="tc ph5 lh-copy">
-              <hh1 class="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple">404</hh1><br></br>
-              <hh2 class="tc f1-l fw1">Sorry, we can't find the page you are looking for.</hh2>
-          </header>
-          
-          <ul class="list tc pl0 w-100 mt5">
-          <li class="dib"><a class="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/"><hh5>Return to Home</hh5></a></li>
-          </ul>
-        </section>
-        <Footer/>
-      </Container>
-    
+  return (
+
+    <Container>
+      <section className="vh-100 bg-washed-blue baskerville" >
+        <header className="tc ph5 lh-copy">
+          <h4 className="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple" style={{ marginTop: "80px" }}>404</h4><br></br>
+          <h4 className="tc f1-l fw1">Sorry, we can't find the page you are looking for.</h4>
+        </header>
+        
+        <ul className="list tc pl0 w-100 mt5">
+          <li className="dib"><a className="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/"><h4><b>Return to Home</b></h4></a></li>
+        </ul>
+      </section>
+      <Footer />
+    </Container>
+
   )
 }
 
